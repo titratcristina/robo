e4e4a3af017cb06c8ea78945407e1765ee1da9c8
Titrat Cristina <titratcristina@gmail.com>
2018-11-20 23:28:19 +0200

    update

--- robo.ino
+++ robo.ino
@@ -1,85 +1,79 @@
-/*------ Arduino Line Fol0er Code----- */
+/*------ Arduino Line Follower Code----- */
 /*-------definning Inputs------*/
 #define LS 12      // left sensor
 #define RS 11      // right sensor
 
 /*-------definning Outputs------*/
-#define AIA 4       // right motor
-#define AIB 5       // right motor
-#define BIA 2       // left motor
-#define BIB 3       // left motor
-
-int speed = 100;
+#define LM1 4       // left motor
+#define LM2 5       // left motor
+#define RM1 2       // right motor
+#define RM2 3       // right motor
 
 void setup()
 {
   pinMode(LS, INPUT);
   pinMode(RS, INPUT);
-  pinMode(BIA, OUTPUT);
-  pinMode(BIB, OUTPUT);
-  pinMode(AIA, OUTPUT);
-  pinMode(AIB, OUTPUT);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
 }
 
 void loop()
 {
-  if (!(analogRead(LS) && analogRead(RS)))     // Move Forward
+  if(digitalRead(LS) && digitalRead(RS))     // Move Forward
   {
-   forward();
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, HIGH);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
   }
   
-  if (!(analogRead(LS)) && analogRead(RS))     // Turn left
+  if(!(digitalRead(LS)) && digitalRead(RS))     // Turn right
   {
-   left();
+    right();
   }
   
-  if (analogRead(LS) && !(analogRead(RS)))     // turn right
+  if(digitalRead(LS) && !(digitalRead(RS)))     // turn left
   {
-   right();
+    left();
   }
   
-  if (analogRead(LS) && analogRead(RS))     // stop
+  if(!(digitalRead(LS)) && !(digitalRead(RS)))     // stop
   {
-   stop();
+   stopNow();
   }
-}
-
-void stop()
-{
-  analogWrite(AIA, 0);
-  analogWrite(AIB, 0);
-  analogWrite(BIA, 0);
-  analogWrite(BIB, 0);
-}
-
-void backward()
-{
-  analogWrite(AIA, 0);
-  analogWrite(AIB, speed);
-  analogWrite(BIA, speed);
-  analogWrite(BIB, 0);
 }
 
 void forward()
 {
-  analogWrite(AIA, speed);
-  analogWrite(AIB, 0);
-  analogWrite(BIA, 0);
-  analogWrite(BIB, speed);
-}
-
-void left()
-{
-  analogWrite(AIA, speed);
-  analogWrite(AIB, 0);
-  analogWrite(BIA, speed);
-  analogWrite(BIB, 0);
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, HIGH);
+  digitalWrite(RM1, HIGH);
+  digitalWrite(RM2, LOW);
 }
 
 void right()
 {
-  analogWrite(AIA, 0);
-  analogWrite(AIB, speed);
-  analogWrite(BIA, 0);
-  analogWrite(BIB, speed); 
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, HIGH);
+  digitalWrite(RM2, LOW);
 }
+
+void left()
+{
+  digitalWrite(LM1, HIGH);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, LOW);
+}
+
+void stopNow()
+{
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, LOW);
+}
+
