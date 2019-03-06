1ddea4c749622e8ad6bcb3eefb7d66640c4c6092
Titrat Cristina <titratcristina@gmail.com>
2019-03-06 11:09:28 +0200

    Update mazeSolver.ino

--- mazeSolver/mazeSolver.ino
+++ mazeSolver/mazeSolver.ino
@@ -1,112 +1,93 @@
-#define IRS 13      // Senzor infra-rosu
 
-#define ldsTrig 6       // OUT - Senzor distanta stanga
-#define ldsEcho 7       // IN - Senzor distanta stanga
+# define LMF 2 // Motor stanga inainte
+# define LMB 3 // Motor stanga inapoi
+# define RMB 4 // Motor dreapta inapoi
+# define RMF 5 // Motor dreapta inainte
 
-#define rdsTrig 8        // OUT - Senzor distanta dreapta
-#define rdsEcho 9        // IN - Senzor distanta dreapta
+# define ldsTrig 6 // OUT - Senzor distanta stanga
+# define ldsEcho 7 // IN - Senzor distanta stanga
+# define rdsTrig 8 // OUT - Senzor distanta dreapta
+# define rdsEcho 9 // IN - Senzor distanta dreapta
+# define fdsTrig 10 // OUT - Senzor distanta fata
+# define fdsEcho 11 // IN - Senzor distanta fata
 
-#define fdsTrig 10       // OUT - Senzor distanta fata
-#define fdsEcho 11       // IN - Senzor distanta fata
-
-#define LM1 4       // Motor stanga
-#define LM2 5       // Motor stanga
-#define RM1 2       // Motor dreapta
-#define RM2 3       // Motor dreapta
-
+# define IRS 13 // Senzor infra-rosu
 
 //Variabilele pentru senzorii de distanta
-int Lduration;
 int Ldistance;
-
-int Rduration;
 int Rdistance;
-
-int Fduration;
 int Fdistance;
 
+int direction; // Variabila pentru alegerea directie de baza (RANDOM)
 
-void setup()
-{
-  pinMode(IRS, INPUT);       // Receptioneaza semnal infra-rosu
-  
-  pinMode(ldsTrig, OUTPUT);       // Trimite semnal distanta stanga
-  pinMode(ldsEcho, INPUT);        // Receptioneaza semnal distanta
+void setup() {
+  pinMode(IRS, INPUT); // Receptioneaza semnal infra-rosu
+
+  pinMode(ldsTrig, OUTPUT); // Trimite semnal distanta stanga
+  pinMode(ldsEcho, INPUT); // Receptioneaza semnal distanta
 
   pinMode(rdsTrig, OUTPUT);
   pinMode(rdsEcho, INPUT);
 
   pinMode(fdsTrig, OUTPUT);
   pinMode(fdsEcho, INPUT);
-  
-  pinMode(LM1, OUTPUT);
-  pinMode(LM2, OUTPUT);
-  pinMode(RM1, OUTPUT);
-  pinMode(RM2, OUTPUT);
 
-  Serial.begin(9600);       // Porneste comunicarea Arduino->Consola PC
+  pinMode(RMB, OUTPUT);
+  pinMode(RMF, OUTPUT);
+  pinMode(LMF, OUTPUT);
+  pinMode(LMB, OUTPUT);
+
+  direction = rand() % 2; // Alegerea unui numar pentru directia de baza in mod aleator (0 sau 1)
+
+  Serial.begin(9600); // Porneste comunicarea Arduino->consola PC
 }
 
+void loop() {
+  // Senzorul IR returneaza 0 => negru (sfarsit)
 
-void loop()
-{
-  // Daca senzorul infra-rosu returneaza valoarea 0 => Senzorul este pe negru
-  // Daca senzorul infra-rosu returneaza valoarea 1 => Senzorul este pe alb
-  
-  if(!(digitalRead(IRS)))       // Verifica daca robotul a ajuns la finish
-  {
   // Initializeaza functiile pentru calculul distantei
   leftDistance();
-  rightDistance();
+  //rightDistance();
   forwardDistance();
+  Serial.println("");
 
-  // Rezolvarea labirintului
-  if (Fdistance > 10 && Ldistance <=10 && Rdistance <=10)
-  {
-    forward();
+  //if(digitalRead(IRS) == 0)     // Verifica daca robotul a ajuns la sfarsit
+  //{ stopNow(); }
+  //else
+  //{
 
-  } else if (Fdistance <=10 && Ldistance >10 && Rdistance <=10) 
-  {
-    left();
-
-  } else if (Fdistance <=10 && Ldistance <=10 && Rdistance >10)
-  {
-    right();
+    
+    if(Ldistance > 5 && Fdistance > 5)
+    goLeft();
+    
+    else
+    turnRight();
+    //}
+    
   
-  } else if (Fdistance >10 && Ldistance >10 && Rdistance >10)
-  {
-    left();
-
-  } else if (Fdistance <=10 && Ldistance <=10 && Rdistance <=10)
-  {
-    fullLeft();
-  }
-
-
-  } else { stopNow(); }       // Robotul a ajuns la finish
   
-}       // Final LOOP
 
+} // Final LOOP
 
-// Calcul distante
-void leftDistance()       // Calculul distantei - Stanga
+// --------------------------------------------------------- Calcul distante
+void leftDistance() // Calculul distantei - Stanga
 {
-  digitalWrite(ldsTrig, LOW);       // Ne asiguram ca senzorul nu este deja pornit
+  digitalWrite(ldsTrig, LOW);
   delayMicroseconds(2);
 
-  digitalWrite(ldsTrig, HIGH);       // Trimite semnal
+  digitalWrite(ldsTrig, HIGH);
   delayMicroseconds(10);
   digitalWrite(ldsTrig, LOW);
 
-  Lduration = pulseIn(ldsEcho, HIGH);
+  int Lduration = pulseIn(ldsEcho, HIGH);
+  Ldistance = Lduration * 0.034 / 2;
 
-  Ldistance = Lduration * 0.034 / 2;        // Formula pentru distanta in centimetri
-
-  Serial.print("Ldistance: ");       // Afiseaza in consola valorile date de senzor
+  Serial.print("Ldistance: ");
   Serial.println(Ldistance);
 }
 
-void rightDistance()       // Calculul distantei - Dreapta
+
+void rightDistance() // Calculul distantei - Dreapta
 {
   digitalWrite(rdsTrig, LOW);
   delayMicroseconds(2);
@@ -115,16 +96,14 @@ void rightDistance()       // Calculul distantei - Dreapta
   delayMicroseconds(10);
   digitalWrite(rdsTrig, LOW);
 
-
-  Rduration = pulseIn(rdsEcho, HIGH);
-
+  int Rduration = pulseIn(rdsEcho, HIGH);
   Rdistance = Rduration * 0.034 / 2;
 
   Serial.print("Rdistance: ");
   Serial.println(Rdistance);
 }
 
-void forwardDistance()       // Calculul distantei - Inainte
+void forwardDistance() // Calculul distantei - Inainte
 {
   digitalWrite(fdsTrig, LOW);
   delayMicroseconds(2);
@@ -133,9 +112,7 @@ void forwardDistance()       // Calculul distantei - Inainte
   delayMicroseconds(10);
   digitalWrite(fdsTrig, LOW);
 
-
-  Fduration = pulseIn(fdsEcho, HIGH);
-
+  int Fduration = pulseIn(fdsEcho, HIGH);
   Fdistance = Fduration * 0.034 / 2;
 
   Serial.print("Fdistance: ");
@@ -143,51 +120,45 @@ void forwardDistance()       // Calculul distantei - Inainte
 }
 
 
-// Miscarile robotului
-void forward()
-{
-   digitalWrite(LM1, LOW);
-  digitalWrite(LM2, HIGH);
-  digitalWrite(RM1, HIGH);
-  digitalWrite(RM2, LOW);
+void goForward() { // ----------------------------------- Miscarile robotului
+  digitalWrite(RMF, HIGH);
+  digitalWrite(LMF, HIGH);
+  digitalWrite(RMB, LOW);
+  digitalWrite(LMB, LOW);
 }
 
-void right()
-{
-  digitalWrite(LM1, HIGH);
-  digitalWrite(LM2, LOW);
-  digitalWrite(RM1, LOW);
-  digitalWrite(RM2, LOW);
+void goRight() {
+  digitalWrite(RMF, LOW);
+  digitalWrite(LMF, HIGH);
+  digitalWrite(RMB, LOW);
+  digitalWrite(LMB, LOW);
 }
 
-void left()
-{
-  digitalWrite(LM1, LOW);
-  digitalWrite(LM2, LOW);
-  digitalWrite(RM1, LOW);
-  digitalWrite(RM2, HIGH);
+void goLeft() {
+  digitalWrite(RMF, HIGH);
+  digitalWrite(LMF, LOW);
+  digitalWrite(RMB, LOW);
+  digitalWrite(LMB, LOW);
 }
 
-void fullLeft()
-{
-  digitalWrite(LM1, LOW);
-  digitalWrite(LM2, HIGH);
-  digitalWrite(RM1, LOW);
-  digitalWrite(RM2, HIGH);
+void turnRight() {
+  digitalWrite(RMF, LOW);
+  digitalWrite(LMF, HIGH);
+  digitalWrite(RMB, HIGH);
+  digitalWrite(LMB, LOW);
 }
 
-void fullRight()
-{
-  digitalWrite(LM1, HIGH);
-  digitalWrite(LM2, LOW);
-  digitalWrite(RM1, HIGH);
-  digitalWrite(RM2, LOW);
+void turnLeft() {
+  digitalWrite(RMF, HIGH);
+  digitalWrite(LMF, LOW);
+  digitalWrite(RMB, LOW);
+  digitalWrite(LMB, HIGH);
 }
 
-void stopNow()
-{
-  digitalWrite(LM1, LOW);
-  digitalWrite(LM2, LOW);
-  digitalWrite(RM1, LOW);
-  digitalWrite(RM2, LOW);
+void stopNow() {
+  digitalWrite(RMF, LOW);
+  digitalWrite(LMF, LOW);
+  digitalWrite(RMB, LOW);
+  digitalWrite(LMB, LOW);
 }
+
